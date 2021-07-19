b950bf65f44d4794d0d0d3a794ddb61673bdd0d8
muzig <nxls4605060315@gmail.com>
2021-07-19 22:44:25 +0800

    [crawler] parse index json

---
+++ cmd/crawler/index.py
@@ -0,0 +1,35 @@
+#!/bin/env python3
+# coding=utf-8
+
+from bs4 import BeautifulSoup
+import pprint
+
+soup = BeautifulSoup(open('../src/index.html', 'r'), 'lxml')
+
+collector = []
+for tag in soup.find_all('li'):
+    if tag.get('class') is None:
+        continue
+    if "ui-slide-item" in tag.get('class'):
+        # 解析基本信息
+        data = {
+            "title": tag.get('data-title'),
+            "release": tag.get('data-release'),
+            "director": tag.get('data-director'),
+            "actors": tag.get('data-actors'),
+        }
+        if data['title'] is None:
+            continue
+        # 解析图片信息
+        img = tag.find('img')
+        if img is None:
+            continue
+        if img.get('src') is None:
+            continue
+        data['img'] = img.get('src')
+        collector.append(data)
+
+# pprint.pprint(collector)
+
+with open('../src/index.json', 'w') as f:
+    f.write(collector.__str__().replace('\'','\"'))
--- cmd/crawler/index/index.py
+++
@@ -1,7 +0,0 @@
-#!/bin/env python3
-# coding=utf-8
-
-from bs4 import BeautifulSoup
-import requests
-
-# requests.get()
